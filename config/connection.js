d8bdc6f698745d9d50fc5c8c738d63c64a61f3bd
aali9 <74592306+aali9@users.noreply.github.com>
2022-08-14 15:34:58 +0100

    connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,21 @@
 
 const mysql = require ('mysql2');
+
+//connect to the database
+
+const db =mysql.createConnection(
+ {
+    host: 'localhost',
+    user: 'root',
+
+    password: '',
+    databse: 'employeedata_db'
+ },
+  console.log('Connected to the employeedata_db.')
+);
+
+db.connect((err) => {
+    if (err) throw err;
+})
+
+module.exports = db
